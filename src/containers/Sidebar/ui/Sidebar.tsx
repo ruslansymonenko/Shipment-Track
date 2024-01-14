b0e155192351fade5ebb3687354e41af8ba0cf56
Ruslan Symonenko <symonenkors@gmail.com>
2024-01-14 22:47:39 +0200

    added image to sidebar

--- src/containers/Sidebar/ui/Sidebar.tsx
+++ src/containers/Sidebar/ui/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
 
@@ -6,10 +6,32 @@ import { addClassNames } from '../../../utils/addClassNames/addClassNames';
 
 import './Sidebar.scss';
 
+import sidebarImgPrimary from '../../../assets/icons/delivery-primary.svg';
+import sidebarImgSecondary from '../../../assets/icons/delivery-secondary.svg';
+import { ThemeContext } from '../../../providers/ThemeProvider/ui/ThemeProvider';
+
 const Sidebar: React.FC = () => {
   const isActive = useSelector((state: RootState) => state.sidebar.isActive);
+  const [sidebarImg, setSidebarImg] = useState(sidebarImgPrimary);
+  const { theme } = useContext(ThemeContext);
 
-  return <div className={addClassNames('sidebar', { collapsed: isActive })}>Sidebar</div>;
+  useEffect(() => {
+    if (theme === 'normal') {
+      setSidebarImg(sidebarImgSecondary);
+    } else {
+      setSidebarImg(sidebarImgPrimary);
+    }
+  }, [theme]);
+
+  return (
+    <div className={addClassNames('sidebar', { collapsed: isActive })}>
+      <img
+        className="sidebar-img"
+        src={sidebarImg}
+        alt="truck"
+      />
+    </div>
+  );
 };
 
 export default Sidebar;
